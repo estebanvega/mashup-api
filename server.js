cc731a23ab30d1e8a15dbf1a73616c38de7a4737
Esteban Vega <esteban.veg4@gmail.com>
2017-11-25 13:43:19 +0100

    Setup API route & render link on base route

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 
+/**
+ * Controllers
+ */
+const apiController = require('./controllers/api');
+
+/**
+ * Create Express server.
+ */
 const app = express();
 const port = 8000;
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('<a href=\"/api/artist\">API example</a>');
 });
 
+app.get('/api/artist', apiController.getArtist);
+
+/**
+ * Start Express server.
+ */
 app.listen(port, () => {
-  console.log('We are live on ' + port);
+    console.log('We are live on ' + port);
 });
+
+module.exports = app;
