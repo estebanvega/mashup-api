e3c4fc55986009249528d97d416caf203e923c27
Esteban Vega <esteban.veg4@gmail.com>
2017-11-25 16:31:28 +0100

    Fix import and update expected response

--- test/app.js
+++ test/app.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./server.js').app;
+const app = require('../server.js').app;
 const server = require('../server.js').server;
 
 describe('GET /api/artist', () => {
@@ -14,12 +14,13 @@ describe('GET /api/artist', () => {
       .expect(200, done);
   });
 
-  it('should return artist name', done => {
+  it('should return JSON response', done => {
     request(app)
       .get('/api/artist')
       .expect(
         200,
         {
+          mbid: '81739b1e-609d-4a63-beaa-4422ca5e278a',
           name: 'Le Knight Club'
         },
         done
