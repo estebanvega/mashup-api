b790476fb230b64f175a6cefd80be36e0e488975
Esteban Vega <esteban.veg4@gmail.com>
2017-11-25 16:15:01 +0100

    Add /api/musicbrainz route & use bodyparser

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 /**
  * Controllers
  */
 const apiController = require('./controllers/api');
 
+
 /**
  * Create Express server.
  */
 const app = express();
-const port = 8000;
 
+
+/**
+ * Express configuration.
+ */
+const port = 8000;
 app.set('host', '0.0.0.0');
 app.set('port', port);
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
   res.send('<a href="/api/artist">API example</a>');
 });
 
 app.get('/api/artist', apiController.getArtist);
+app.get('/api/musicbrainz/:id', apiController.getMusicBrainz);
 
 /**
  * Start Express server.
