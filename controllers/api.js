3cbce61c9310b95900ae977e4f901c0a8999eccd
Esteban Vega <esteban.veg4@gmail.com>
2017-11-25 16:14:00 +0100

    Add route to request MusicBrainz via ID

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,39 @@
+const rp = require('request-promise');
+const urljoin = require('url-join');
+
 /**
  * GET /api/artist
  */
 exports.getArtist = (req, res) => {
   res.status(200).json({ name: 'Le Knight Club' });
+};
+
+/**
+ * GET /api/musicbrainz/id
+ */
+exports.getMusicBrainz = (req, res, next) => {
+  const baseUrl = 'https://musicbrainz.org/ws/2/artist/';
+  const url = urljoin(baseUrl, req.params.id);
+
+  const options = {
+    uri: url,
+    qs: {
+      fmt: 'json',
+      inc: 'url-rels+release-groups'
+    },
+    headers: {
+      'User-Agent': 'mashup-api'
+    },
+    resolveWithFullResponse: false,
+    json: false
+  };
+
+  rp(options)
+    .then(body => {
+      const parsedBody = JSON.parse(body);
+      return next(parsedBody.name);
+    })
+    .catch(err => {
+      return next(err);
+    });
 };
