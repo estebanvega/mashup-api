dc0a5f713ae9edd4f74f779795603767291fbf42
Esteban Vega <esteban.veg4@gmail.com>
2017-11-25 17:07:27 +0100

    Update test to expect artist description from response

--- test/app.js
+++ test/app.js
@@ -12,6 +12,7 @@ describe('GET /api/artist/:mbid', () => {
   it('should return JSON response', done => {
     const artistId = '81739b1e-609d-4a63-beaa-4422ca5e278a';
     const reqUrl = urljoin('/api/artist/', artistId);
+    const desc = '<p><b>Guillaume Emmanuel "Guy-Manuel" de Homem-Christo</b> (<small>French pronunciation: ​</small><span title="Representation in the International Phonetic Alphabet (IPA)">[ɡi manɥɛl də ɔmɛm kʁisto]</span>; born 8 February 1974) is a French musician, record producer, singer, songwriter, DJ and film director, best known for being one half of the French house music duo Daft Punk, along with Thomas Bangalter. He has also produced several works from his record label Crydamoure with label co-owner Éric Chedeville. He and Chedeville formed the musical duo <b>Le Knight Club</b>.</p> <p></p> <p></p>';
 
     request(app)
       .get(reqUrl)
@@ -19,7 +20,8 @@ describe('GET /api/artist/:mbid', () => {
         200,
         {
           mbid: artistId,
-          name: 'Le Knight Club'
+          name: 'Le Knight Club',
+          description: desc
         },
         done
       );
